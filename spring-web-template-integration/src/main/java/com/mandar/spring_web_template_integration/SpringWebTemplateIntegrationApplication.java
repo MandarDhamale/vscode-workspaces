717d8da9fa8c492a093d3aa58ba749e718186dc3
Mandar Dhamale <mandardhamale@gmail.com>
2025-02-07 23:36:32 +0530

    Spring MVC Web Template Integration

---
+++ spring-web-template-integration/src/main/java/com/mandar/spring_web_template_integration/SpringWebTemplateIntegrationApplication.java
@@ -0,0 +1,14 @@
+package com.mandar.spring_web_template_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebTemplateIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebTemplateIntegrationApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ spring-web-template-integration/src/test/java/com/mandar/spring_web_template_integration/SpringWebTemplateIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandar.spring_web_template_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebTemplateIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
