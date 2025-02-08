54c864c1035d5dc30213984d0f6ddb54a950995b
Mandar Dhamale <mandardhamale@gmail.com>
2025-02-08 22:26:08 +0530

    displaying post data on UI using thymeleaf

--- spring-web-template-integration/src/main/java/com/mandar/spring_web_template_integration/Controllers/HomeController.java
+++ spring-web-template-integration/src/main/java/com/mandar/spring_web_template_integration/Controllers/HomeController.java
@@ -1,19 +1,39 @@
 package com.mandar.spring_web_template_integration.Controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+import com.mandar.spring_web_template_integration.models.Post;
+import com.mandar.spring_web_template_integration.services.PostService;
 
 @Controller
 public class HomeController {
-    
+
+    @Autowired
+    PostService postService;
+
+
+
+
     @GetMapping("/")
     public String defaultMapping(){
         return "home";
     }
 
     @GetMapping("/home")
-    public String home(){
+    public String home(Model model){
+
+        List<Post> posts = postService.getAll();
+
+        model.addAttribute("_posts", posts);
+
         return "home";
+
+
     }
 
     @GetMapping("/about")
