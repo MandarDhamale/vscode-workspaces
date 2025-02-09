7e47417d86a80a84c44414ef1a410154253731f8
Mandar Dhamale <mandardhamale@gmail.com>
2025-02-09 18:39:42 +0530

    adding account model

---
+++ spring-web-template-integration/src/main/java/com/mandar/spring_web_template_integration/models/Account.java
@@ -0,0 +1,37 @@
+package com.mandar.spring_web_template_integration.models;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String firstname;
+
+    private String lastname;
+
+    //one to many relationship with table Posts -> one account can have one or multiple posts
+    @OneToMany(mappedBy = "account")
+    private List<Post> posts;
+
+
+}
