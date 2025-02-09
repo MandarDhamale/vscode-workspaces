942c469e8b8eb2a3478cd0daf173cfe304df470f
Mandar Dhamale <mandardhamale@gmail.com>
2025-02-09 23:21:18 +0530

    adding user account registration form

---
+++ spring-web-template-integration/src/main/java/com/mandar/spring_web_template_integration/Controllers/AccountController.java
@@ -0,0 +1,39 @@
+package com.mandar.spring_web_template_integration.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.mandar.spring_web_template_integration.models.Account;
+import com.mandar.spring_web_template_integration.services.AccountService;
+
+@Controller
+public class AccountController {
+    
+    @Autowired
+    AccountService accountService;
+
+
+    @GetMapping("/register")
+    public String register(Model model){
+
+        Account account = new Account();
+
+        model.addAttribute("account", account);
+
+        return "register";
+
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute Account account){
+
+        accountService.save(account);
+        return "redirect:/";
+        
+    }
+
+}
