bf189bf4af63314add1d8f66aaf09363ad654330
Mandar Dhamale <mandardhamale@gmail.com>
2025-02-11 11:38:28 +0530

    spring security login - adding essential configuration

--- spring-web-template-integration/src/main/java/com/mandar/spring_web_template_integration/config/WebSecurityConfig.java
+++ spring-web-template-integration/src/main/java/com/mandar/spring_web_template_integration/config/WebSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -18,43 +20,56 @@ public class WebSecurityConfig {
         "/login",
         "/home",
         "/register",
-        "db-console/**",
+        "/db-console/**",
         "/css/**",
         "/fonts/**",
         "/images/**",
         "/js/**",
         "/templates/**"
-
     };
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        
+
         http
-        .authorizeHttpRequests((authz) -> {
+            .authorizeHttpRequests((authz) -> {
 
-            // // Allow access to static resources
-            // authz.requestMatchers(new AntPathRequestMatcher("/css/**")).permitAll();
-            // authz.requestMatchers(new AntPathRequestMatcher("/js/**")).permitAll();
-            // authz.requestMatchers(new AntPathRequestMatcher("/images/**")).permitAll();
-            // authz.requestMatchers(new AntPathRequestMatcher("/fonts/**")).permitAll(); // Add this line
+                for (String pattern : WHITELIST) {
+                    authz.requestMatchers(new AntPathRequestMatcher(pattern)).permitAll();
+                }
 
-            for (String pattern : WHITELIST) {
-                authz.requestMatchers(new AntPathRequestMatcher(pattern)).permitAll();
-            }
-
-            authz
-                .anyRequest().authenticated();
-        });
+                authz
+                    .anyRequest().authenticated();
+            })
+            .formLogin(form -> form
+                .loginPage("/login") // Specify your login page URL
+                .permitAll() // Allow access to the login page for everyone
+                .defaultSuccessUrl("/home") // Redirect to /home after successful login
+                .failureUrl("/login?error") // Redirect to login with error parameter if login fails.
+            )
+            .logout(logout -> logout
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // URL to trigger logout
+                .logoutSuccessUrl("/login?logout") // Redirect to /login after logout.  Include a logout parameter if you want.
+                .permitAll() // Allow everyone to access the logout URL
+                .invalidateHttpSession(true) // Invalidate the HTTP session
+                .deleteCookies("JSESSIONID") // Optionally delete the session cookie
+            );
 
         //TODO: Remove these after upgrading the DB from H2 infile DB
         http.csrf().disable();
         http.headers().frameOptions().disable();
 
 
-            return http.build();
+        return http.build();
 
     }
 
-    
+
 }
--- spring-web-template-integration/src/main/java/com/mandar/spring_web_template_integration/services/AccountService.java
+++ spring-web-template-integration/src/main/java/com/mandar/spring_web_template_integration/services/AccountService.java
@@ -1,6 +1,7 @@
 package com.mandar.spring_web_template_integration.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.mandar.spring_web_template_integration.models.Account;
@@ -12,7 +13,12 @@ public class AccountService {
     @Autowired
     private AccountRepository accountRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
     public Account save(Account account){
+        account.setPassword(passwordEncoder.encode(account.getPassword()));
         return accountRepository.save(account);
     }
 
