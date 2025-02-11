eb835bc31f2c009026d151e6c4a63b790c3bec24
Mandar Dhamale <mandardhamale@gmail.com>
2025-02-11 21:55:20 +0530

    removing unused imports

--- spring-web-template-integration/src/main/java/com/mandar/spring_web_template_integration/Controllers/HomeController.java
+++ spring-web-template-integration/src/main/java/com/mandar/spring_web_template_integration/Controllers/HomeController.java
@@ -3,14 +3,12 @@ package com.mandar.spring_web_template_integration.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 
 import com.mandar.spring_web_template_integration.models.Post;
 import com.mandar.spring_web_template_integration.services.PostService;
-import org.springframework.security.core.Authentication;
 
 @Controller
 public class HomeController {
