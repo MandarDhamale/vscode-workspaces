7b74eec5d818371394eefabeb0965b36f9d637e6
Mandar Dhamale <mandardhamale@gmail.com>
2025-03-31 22:57:52 +0530

    adding overloaded method for sorting and pagination

--- spring-web-template-integration/src/main/java/com/mandar/spring_web_template_integration/services/PostService.java
+++ spring-web-template-integration/src/main/java/com/mandar/spring_web_template_integration/services/PostService.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.mandar.spring_web_template_integration.models.Post;
@@ -22,6 +25,10 @@ public class PostService {
 
     public List<Post> getAll() {
         return postRepository.findAll();
+    }
+
+    public Page<Post> getAll(int offset, int pageSize, String field) {
+        return postRepository.findAll(PageRequest.of(offset, pageSize).withSort(Direction.ASC, field)); //field needs to exactly match the column name
     }
 
     public void delete(Post post) {
