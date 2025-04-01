7bcff496a09abc22ad64551057fd89f35866f223
Mandar Dhamale <mandardhamale@gmail.com>
2025-04-01 23:03:47 +0530

    updating the method for applying filters

--- spring-web-template-integration/src/main/java/com/mandar/spring_web_template_integration/Controllers/HomeController.java
+++ spring-web-template-integration/src/main/java/com/mandar/spring_web_template_integration/Controllers/HomeController.java
@@ -3,8 +3,10 @@ package com.mandar.spring_web_template_integration.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 
 import com.mandar.spring_web_template_integration.models.Post;
@@ -22,14 +24,17 @@ public class HomeController {
     }
 
     @GetMapping("/home")
-    public String home(Model model) {
+    public String home(Model model,
+            @RequestParam(required = false, name = "sortBy", defaultValue = "createdAt") String sortBy,
+            @RequestParam(required = false, name = "perPage", defaultValue = "1") String perPage,
+            @RequestParam(required = false, name = "page", defaultValue = "1") String page) {
 
-        List<Post> posts = postService.getAll();
+        // List<Post> posts = postService.getAll();
+        // model.addAttribute("posts", posts);
 
-        model.addAttribute("posts", posts);
+        Page<Post> postsOnPage = postService.getAll(Integer.parseInt(page)-1, Integer.parseInt(perPage), sortBy);
 
         return "home_views/home";
-
     }
 
 }
