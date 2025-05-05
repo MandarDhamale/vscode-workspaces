293d00bb171e9f1306eed8d30969b420e3e118b8
Mandar Dhamale <mandardhamale@gmail.com>
2025-05-05 23:18:14 +0530

    adding jwt decoder

--- SpringRestDemo/src/main/java/com/mandar/SpringRestDemo/config/SwaggerConfig.java
+++ SpringRestDemo/src/main/java/com/mandar/SpringRestDemo/config/SwaggerConfig.java
@@ -1,5 +1,30 @@
 package com.mandar.SpringRestDemo.config;
 
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Demo API",
+        version = "Version 1.0",
+        contact = @Contact(
+            name = "mandar", email="mandardhamale@gmail.com", url = "https://mandar.com"
+        ),
+        license = @License(
+            name = "Apache 2.0", url = "https://www.apache.org/licenses/"
+        ),
+        termsOfService = "https://mandar.com",
+        description = "Spring Boot RestFul API demo"
+    )
+)
+
+
+
 public class SwaggerConfig {
     
 }
--- SpringRestDemo/src/main/java/com/mandar/SpringRestDemo/security/SecurityConfig.java
+++ SpringRestDemo/src/main/java/com/mandar/SpringRestDemo/security/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,7 +20,7 @@ public class SecurityConfig {
 
         return new InMemoryUserDetailsManager(
 
-                User.withUsername("mandar")
+                User.withUsername("mrd")
                         .password("{noop}mrd")
                         .authorities("read")
                         .build()
@@ -33,7 +35,10 @@ public class SecurityConfig {
                 .requestMatchers("/").permitAll()
                 .requestMatchers("/swagger-ui/**").permitAll()
                 .requestMatchers("/v3/api-docs/**").permitAll()
-                .requestMatchers("/test").authenticated();
+                .requestMatchers("/test").authenticated()
+                .and()
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
