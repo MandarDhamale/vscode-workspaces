7acbee631060c9d885cb495f701d11c50f5ff6c6
Mandar Dhamale <mandardhamale@gmail.com>
2025-06-21 19:34:35 +0530

    adding jwt encoder and decoder

--- SpringRestApi/src/main/java/com/mandar/SpringRestApi/security/SecurityConfig.java
+++ SpringRestApi/src/main/java/com/mandar/SpringRestApi/security/SecurityConfig.java
@@ -7,35 +7,66 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.mandar.SpringRestApi.config.RSAKeyProperties;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    public InMemoryUserDetailsManager users() {
-        return new InMemoryUserDetailsManager(
-                User.withUsername("mrd")
-                        .password("{noop}mrd")
-                        .authorities("read")
-                        .build());
-    }
+        private final RSAKeyProperties rsaKeys;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        public SecurityConfig(RSAKeyProperties rsaKeys) {
+                this.rsaKeys = rsaKeys;
+        }
 
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/token", "/", "/swagger-ui/**", "/v3/api-docs/**", "/test2").permitAll()
-                        .requestMatchers("/test").authenticated())
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Stateless session
-                )
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt); // Enable JWT
+        public InMemoryUserDetailsManager users() {
+                return new InMemoryUserDetailsManager(
+                                User.withUsername("mrd")
+                                                .password("{noop}mrd")
+                                                .authorities("read")
+                                                .build());
+        }
 
-        return http.build();
+        @Bean
+        JwtDecoder jwtDecoder() {
+                return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
+        }
 
-    }
+        @Bean
+        JwtEncoder jwtEncoder(){
+                JWK jwk = new RSAKey.Builder(rsaKeys.publicKey()).privateKey(rsaKeys.privateKey()).build();
+                JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+                return new NimbusJwtEncoder(jwks);
+        }
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+                http
+                                .authorizeHttpRequests(auth -> auth
+                                                .requestMatchers("/token", "/", "/swagger-ui/**", "/v3/api-docs/**",
+                                                                "/test2")
+                                                .permitAll()
+                                                .requestMatchers("/test").authenticated())
+                                .sessionManagement(session -> session
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Stateless
+                                                                                                        // session
+                                )
+                                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt); // Enable JWT
+
+                return http.build();
+
+        }
 
 }
