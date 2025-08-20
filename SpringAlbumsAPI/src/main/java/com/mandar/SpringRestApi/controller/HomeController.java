14da0d3c9e04bb20c7cae8cc8c800242cf77fa7e
Mandar Dhamale <mandardhamale@gmail.com>
2025-08-20 12:29:54 -0400

    api-docs and cors policy update

--- SpringAlbumsAPI/src/main/java/com/mandar/SpringRestApi/controller/HomeController.java
+++ SpringAlbumsAPI/src/main/java/com/mandar/SpringRestApi/controller/HomeController.java
@@ -5,10 +5,7 @@ import com.mandar.SpringRestApi.service.AccountService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -17,6 +14,7 @@ import java.util.List;
 
 @RestController
 @Tag(name = "Home Controller", description = "Controller for home management")
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600);
 public class HomeController {
 
     @Autowired
